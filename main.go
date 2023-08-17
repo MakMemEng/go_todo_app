9963f814d4cf173e417baf48426bc9ae582c552a
MakMemEng <yatani.mems4568@gmail.com>
2023-08-18 08:14:36 +0900

    ✨ section-056 ポート番号を動的に変更

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 
 	"golang.org/x/sync/errgroup"
@@ -17,16 +18,20 @@ func main() {
 
 // 割り当てたいポート番号が既に利用されている場合，競合してエラーが発生
 // 動的にポート番号を変更してrun関数を起動する
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
+		// 引数で受け取ったnet.Listenerを利用するので
+		// Addrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
+		// ListenAndServerメソッドではなく，Serveメソッドへ変更
+		if err := s.Serve(l); err != nil &&
+		// http.ErrServerClosedは
+		// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
 		err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
