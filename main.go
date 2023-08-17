6218c88a185b8b2a1a3892891fb3d6d47ef84ab0
MakMemEng <yatani.mems4568@gmail.com>
2023-08-18 08:22:24 +0900

    ✨ 実行時の引数でポート番号を指定

--- main.go
+++ main.go
@@ -6,13 +6,26 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
+// os.Args変数を使用して実行時の引数でポート番号を指定
+// ex: go run . 18080
 func main() {
-	if err := run(context.Background()); err != nil {
-	log.Printf("failed to terminate server: %v", err)
+	if (len(os.Args)) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
