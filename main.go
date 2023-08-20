b9b1fc6c5ee96c15751c32504f80cb71e2225855
MakMemEng <yatani.mems4568@gmail.com>
2023-08-20 10:04:47 +0900

    🔐 環境変数から設定をロードする

--- main.go
+++ main.go
@@ -8,30 +8,37 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/MakMemEng/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
 // os.Args変数を使用して実行時の引数でポート番号を指定
 // ex: go run . 18080
 func main() {
-	if (len(os.Args)) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
 
+// context.Context型の値を引数にとり，
+// 外部からのキャンセル操作を受け取った際にサーバーを終了するように実装
+// 異常時にはerror型の値を返す,func run(ctx context.Context) error関数を実装
+
 // 割り当てたいポート番号が既に利用されている場合，競合してエラーが発生
 // 動的にポート番号を変更してrun関数を起動する
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	// *http.Server型を経由してHTTPサーバーを起動
 	s := &http.Server{
 		// 引数で受け取ったnet.Listenerを利用するので
 		// Addrフィールドは指定しない
@@ -40,6 +47,7 @@ func run(ctx context.Context, l net.Listener) error {
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
+	// 別ゴルーチンでHTTPサーバーを起動
 	eg.Go(func() error {
 		// ListenAndServerメソッドではなく，Serveメソッドへ変更
 		if err := s.Serve(l); err != nil &&
@@ -52,9 +60,11 @@ func run(ctx context.Context, l net.Listener) error {
 		return nil
 	})
 
+	// チャネルからの終了通知を待機
 	<-ctx.Done()
 	if err := s.Shutdown(context.Background()); err != nil {
 		log.Printf("failed to shutdown: %+v", err)
 	}
+	// Goメソッドで起動した別ゴルーチンの終了待機
 	return eg.Wait()
 }
